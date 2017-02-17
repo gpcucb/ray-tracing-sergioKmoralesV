82bf52f937c122a406b63a16a3c3e4d6e07d37d0
sergioKmoralesV <s.mora.94@gmail.com>
2017-02-16 20:14:04 -0400

    RGB class created

--- rgb_test.rb
+++ rgb_test.rb
@@ -1,8 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
+require './rgb'
 
 MiniTest::Reporters.use!
 
 class RGBTest < Minitest::Test
+  def setup
+    @a_rgb = RGB.new()
+  end
 
+  def test_01_creates_an_object_rgb
+  end
 end
