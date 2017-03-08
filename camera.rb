fe4dac818a17f8a193dd614ff3ab9c23c60ebc0b
sergioKmoralesV <s.mora.94@gmail.com>
2017-03-07 20:46:49 -0400

    vector u calculating, not tested

--- camera.rb
+++ camera.rb
@@ -6,10 +6,22 @@ class Camera
     @up = up
   end
 
-  def vector_w()
+  def calculate_w_vector
     x = @e.x/@e.module
     y = @e.y/@e.module
     z = @e.z/@e.module
     Vector.new(x, y, z)
+  end
+
+  def calculate_u_vector a_w_vector
+    a_product = @up.cross_product(a_w_vector)
+    x = a_product.x / a_product.module
+    y = a_product.y / a_product.module
+    z = a_product.z / a_product.module
+    Vector.new(x, y, z)
+  end
+
+  def calculate_v_vector (a_w_vector,a_u_vector)
+    a_w_vector.cross_product(a_u_vector)
   end
 end
